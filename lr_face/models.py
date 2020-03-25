a23c91b1c8965c7fbb983b5094a708077aa3e07f
Judith <judith.vdwetering@gmail.com>
2020-03-25 20:09:38 +0100

    comments verwerkt

--- lr_face/models.py
+++ lr_face/models.py
@@ -17,9 +17,9 @@ class DummyModel:
         pass
 
     def predict_proba(self, X):
-        assert X.shape[2:4] == self.resolution
+        # assert X.shape[2:4] == self.resolution
         assert X.shape[1] == 2, f'should get n pairs, but second dimension is {X.shape[1]}'
-        return np.random.random((len(X),2))
+        return np.random.random((len(X), 2))
 
 
 class Deepface_Lib_Model:
