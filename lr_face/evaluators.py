46a4975986edd8303a40779fed7da3d3fbc0bbcd
Jeannette Leegwater <Jeannette@Jeannettes-MacBook-Pro.local>
2020-03-31 14:11:28 +0200

    zelfde commits als in branch feature/data_exploration

--- lr_face/evaluators.py
+++ lr_face/evaluators.py
@@ -25,6 +25,30 @@ def plot_lr_distributions(predicted_log_lrs, y, savefig=None, show=None):
         plt.show()
 
 
+def plot_tippett(predicted_log_lrs, y, savefig=None, show=None):
+    """
+    Plots the 10log lrs in a Tippett plot
+    """
+    xplot = np.linspace(np.min(predicted_log_lrs), np.max(predicted_log_lrs), 100)
+    lr_0, lr_1 = Xy_to_Xn(predicted_log_lrs, y)
+    perc0 = (sum(i > xplot for i in lr_0) / len(lr_0)) * 100
+    perc1 = (sum(i > xplot for i in lr_1) / len(lr_1)) * 100
+
+    plt.figure(figsize=(10, 10), dpi=100)
+    plt.plot(xplot, perc1, color='b', label='LRs given $\mathregular{H_1}$')
+    plt.plot(xplot, perc0, color='r', label='LRs given $\mathregular{H_2}$')
+    plt.axvline(x=0, color='k', linestyle='--')
+    plt.xlabel('Log likelihood ratio')
+    plt.ylabel('Cumulative proportion')
+    plt.title('Tippett plot')
+    plt.legend()
+    if savefig is not None:
+        plt.savefig(savefig)
+        plt.close()
+    if show or savefig is None:
+        plt.show()
+
+
 def calculate_metrics_dict(scores, y, lr_predicted, label):
     """
     Calculates metrics for an lr system
@@ -53,7 +77,7 @@ def evaluate(lr_system: CalibratedScorer, data_provider: ImagePairs, make_plots_
             calibrator = calibrator.first_step_calibrator
         plot_score_distribution_and_calibrator_fit(calibrator, scores, data_provider.y_test, savefig=f'{make_plots_and_save_as} calibration.png')
         plot_lr_distributions(np.log10(LR_predicted),data_provider.y_test,savefig=f'{make_plots_and_save_as} lr distribution.png')
-
+        plot_tippett(np.log10(LR_predicted), data_provider.y_test, savefig=f'{make_plots_and_save_as} tippett.png')
 
     metric_dict = calculate_metrics_dict(scores, data_provider.y_test, LR_predicted, '')
 
