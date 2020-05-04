417d7c860ff7d9dd3760a8895804b8b84cefc24f
Jeroen Mandersloot <jeroen.mandersloot@gmail.com>
2020-05-04 17:33:08 +0200

    Updated params.py for better defaults for this experiment

--- params.py
+++ params.py
@@ -56,7 +56,7 @@ PARAMS = {
 }
 
 DATA = {
-    'current_set_up': ['enfsi'],
+    'current_set_up': ['lfw_sanity_check'],
     'all': {
         # Either specify a single dataset as `datasets`, in which case the
         # dataset is split into calibration and test pairs according to the
@@ -98,7 +98,7 @@ For the input of an experiment the 'current_set_up' list can be updated.
 """
 
 SCORERS = {
-    'current_set_up': ['dummy'],
+    'current_set_up': ['vggface'],
     'all': {
         # We apply lazy loading to the scorer models since they take up a lot
         # of memory. Each setup has type `Tuple[Architecture, Optional[str]]`.
