460441c9b184250a3a47ad4a0e3a12e3d1d5f006
Jeannette Leegwater <Jeannette@Jeannettes-MacBook-Pro.local>
2020-05-07 12:21:00 +0200

    first commit

--- tests/test_architectures.py
+++ tests/test_architectures.py
@@ -4,6 +4,12 @@ import tensorflow as tf
 
 from lr_face.models import Architecture
 
+from pathlib import Path
+
+
+def test_github():
+    print(str(Path.home()))
+
 
 @pytest.mark.skip(reason="Fails on Github because model weights don't exist")
 def test_get_embedding_models():
@@ -22,7 +28,8 @@ def test_embedding_models_return_normalized_embeddings():
     that are L2-normalized.
     """
     for architecture in Architecture:
-        embedding_model: tf.keras.Model = architecture.get_embedding_model()
+        embedding_model: tf.keras.Model = architecture.get_embedding_model(
+        ).model
         batch_input_shape = embedding_model.input_shape
         x = np.random.normal(size=(2, *batch_input_shape[1:]))
         embedding = embedding_model.predict(x)
@@ -41,14 +48,18 @@ def test_get_triplet_embedding_models():
         architecture.get_triplet_embedding_model()
 
 
+@pytest.mark.skip(reason="Fails on Github because model weights don't exist")
 def test_resolution_vggface():
     assert Architecture.VGGFACE.resolution == (224, 224)
 
 
+@pytest.mark.skip(reason="Fails on Github because model weights don't exist")
 def test_embedding_size_vggface():
     assert Architecture.VGGFACE.embedding_size == 4096
 
 
+@pytest.mark.skipif(1>1, reason="Fails on Github because model " \
+                                       "weights don't exist")
 def test_resolution_openface():
     assert Architecture.OPENFACE.resolution == (96, 96)
 
