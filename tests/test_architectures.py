6d93f27f4a569ef86885e73814d5b7aa60fcd68a
Jeannette Leegwater <Jeannette@Jeannettes-MacBook-Pro.local>
2020-05-07 12:51:56 +0200

    test3

--- tests/test_architectures.py
+++ tests/test_architectures.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 @pytest.mark.skipif(str(Path.home())[:13] == '/home/runner/', reason="Fails on Github because model weights don't exist")
 def test_github():
+    print(str(Path.home()))
     assert 1>2
 
 
